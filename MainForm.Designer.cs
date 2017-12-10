00a58692902906b21cda1ba91f0dbad7ff03e878
Thomas Baumer <thomas.baumer@stud.uni-regensburg.de>
2017-12-10 22:25:17 +0100

    init

---
+++ MainForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace GameOfLife
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.BTNTick = new System.Windows.Forms.Button();
+            this.LabelOutput = new System.Windows.Forms.Label();
+            this.BTNAutoTick = new System.Windows.Forms.Button();
+            this.BTNReset = new System.Windows.Forms.Button();
+            this.BTNExit = new System.Windows.Forms.Button();
+            this.LabelRow = new System.Windows.Forms.Label();
+            this.LabelCol = new System.Windows.Forms.Label();
+            this.CBRow = new System.Windows.Forms.ComboBox();
+            this.CBCol = new System.Windows.Forms.ComboBox();
+            this.BTNOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTNTick
+            // 
+            this.BTNTick.Location = new System.Drawing.Point(12, 532);
+            this.BTNTick.Name = "BTNTick";
+            this.BTNTick.Size = new System.Drawing.Size(75, 23);
+            this.BTNTick.TabIndex = 0;
+            this.BTNTick.Text = "Tick";
+            this.BTNTick.UseVisualStyleBackColor = true;
+            this.BTNTick.Click += new System.EventHandler(this.BTNTick_Click);
+            // 
+            // LabelOutput
+            // 
+            this.LabelOutput.AutoSize = true;
+            this.LabelOutput.Font = new System.Drawing.Font("Consolas", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelOutput.Location = new System.Drawing.Point(12, 9);
+            this.LabelOutput.Name = "LabelOutput";
+            this.LabelOutput.Size = new System.Drawing.Size(154, 56);
+            this.LabelOutput.TabIndex = 1;
+            this.LabelOutput.Text = "Space";
+            this.LabelOutput.Click += new System.EventHandler(this.LabelOutput_Click);
+            // 
+            // BTNAutoTick
+            // 
+            this.BTNAutoTick.Location = new System.Drawing.Point(93, 532);
+            this.BTNAutoTick.Name = "BTNAutoTick";
+            this.BTNAutoTick.Size = new System.Drawing.Size(75, 23);
+            this.BTNAutoTick.TabIndex = 2;
+            this.BTNAutoTick.Text = "Auto Tick";
+            this.BTNAutoTick.UseVisualStyleBackColor = true;
+            this.BTNAutoTick.Click += new System.EventHandler(this.BTNAutoTick_Click);
+            // 
+            // BTNReset
+            // 
+            this.BTNReset.Location = new System.Drawing.Point(644, 532);
+            this.BTNReset.Name = "BTNReset";
+            this.BTNReset.Size = new System.Drawing.Size(75, 23);
+            this.BTNReset.TabIndex = 3;
+            this.BTNReset.Text = "Reset";
+            this.BTNReset.UseVisualStyleBackColor = true;
+            this.BTNReset.Click += new System.EventHandler(this.BTNReset_Click);
+            // 
+            // BTNExit
+            // 
+            this.BTNExit.Location = new System.Drawing.Point(725, 532);
+            this.BTNExit.Name = "BTNExit";
+            this.BTNExit.Size = new System.Drawing.Size(75, 23);
+            this.BTNExit.TabIndex = 4;
+            this.BTNExit.Text = "Exit";
+            this.BTNExit.UseVisualStyleBackColor = true;
+            this.BTNExit.Click += new System.EventHandler(this.BTNExit_Click);
+            // 
+            // LabelRow
+            // 
+            this.LabelRow.AutoSize = true;
+            this.LabelRow.Location = new System.Drawing.Point(330, 508);
+            this.LabelRow.Name = "LabelRow";
+            this.LabelRow.Size = new System.Drawing.Size(46, 17);
+            this.LabelRow.TabIndex = 5;
+            this.LabelRow.Text = "Rows:";
+            // 
+            // LabelCol
+            // 
+            this.LabelCol.AutoSize = true;
+            this.LabelCol.Location = new System.Drawing.Point(310, 538);
+            this.LabelCol.Name = "LabelCol";
+            this.LabelCol.Size = new System.Drawing.Size(66, 17);
+            this.LabelCol.TabIndex = 6;
+            this.LabelCol.Text = "Columns:";
+            // 
+            // CBRow
+            // 
+            this.CBRow.FormattingEnabled = true;
+            this.CBRow.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.CBRow.Location = new System.Drawing.Point(382, 501);
+            this.CBRow.Name = "CBRow";
+            this.CBRow.Size = new System.Drawing.Size(121, 24);
+            this.CBRow.TabIndex = 8;
+            // 
+            // CBCol
+            // 
+            this.CBCol.FormattingEnabled = true;
+            this.CBCol.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.CBCol.Location = new System.Drawing.Point(382, 531);
+            this.CBCol.Name = "CBCol";
+            this.CBCol.Size = new System.Drawing.Size(121, 24);
+            this.CBCol.TabIndex = 9;
+            // 
+            // BTNOk
+            // 
+            this.BTNOk.Location = new System.Drawing.Point(509, 532);
+            this.BTNOk.Name = "BTNOk";
+            this.BTNOk.Size = new System.Drawing.Size(75, 23);
+            this.BTNOk.TabIndex = 10;
+            this.BTNOk.Text = "OK";
+            this.BTNOk.UseVisualStyleBackColor = true;
+            this.BTNOk.Click += new System.EventHandler(this.BTNOk_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 567);
+            this.Controls.Add(this.BTNOk);
+            this.Controls.Add(this.CBCol);
+            this.Controls.Add(this.CBRow);
+            this.Controls.Add(this.LabelCol);
+            this.Controls.Add(this.LabelRow);
+            this.Controls.Add(this.BTNExit);
+            this.Controls.Add(this.BTNReset);
+            this.Controls.Add(this.BTNAutoTick);
+            this.Controls.Add(this.LabelOutput);
+            this.Controls.Add(this.BTNTick);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Game of Life";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTNTick;
+        private System.Windows.Forms.Label LabelOutput;
+        private System.Windows.Forms.Button BTNAutoTick;
+        private System.Windows.Forms.Button BTNReset;
+        private System.Windows.Forms.Button BTNExit;
+        private System.Windows.Forms.Label LabelRow;
+        private System.Windows.Forms.Label LabelCol;
+        private System.Windows.Forms.ComboBox CBRow;
+        private System.Windows.Forms.ComboBox CBCol;
+        private System.Windows.Forms.Button BTNOk;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,443 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    /// <summary>
+    /// The Game of Life view and controller.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Stores the "world" of Game of Life in a 2d Boolean array.
+        /// </summary>
+        private Boolean[,] space;
+
+        /// <summary>
+        /// The label which contains the output of the program.
+        /// </summary>
+        private Label displayArea;
+
+        /// <summary>
+        /// The time for automatic ticking.
+        /// </summary>
+        private Timer timer;
+
+        /// <summary>
+        /// Main entrance of the Game of Life form.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+            start();
+        }
+
+        /// <summary>
+        /// Triggers the setup and prints the initial space.
+        /// </summary>
+        private void start()
+        {
+            setup(GameConfig.Default.rows, GameConfig.Default.cols);
+            printSpace(true);
+        }
+
+        /// <summary>
+        /// Setup of the game. 
+        /// Defines and fills the space with random values.
+        /// Setup of the display area.
+        /// </summary>
+        /// <param name="rows">The size of rows which the space will get.</param>
+        /// <param name="columns">The size of cols which the space will get.</param>
+        private void setup(int rows, int columns)
+        {
+            //setup space
+            space = new Boolean[rows, columns];
+            randomizeSpace();
+
+            //setup the area where the output will be shown.
+            Control control = this.Controls["LabelOutput"];
+            displayArea = (Label)control;
+        }
+
+        /// <summary>
+        /// Fills the values of the space array with random values.
+        /// </summary>
+        private void randomizeSpace()
+        {
+            //Built in random generator
+            Random random = new Random();
+
+            for (int rows=0; rows<space.GetLength(0); rows++)
+            {
+                for(int cols=0; cols<space.GetLength(1); cols++)
+                {
+                    //decision
+                    if (random.NextDouble() > 0.5)
+                    {
+                        space[rows, cols] = true;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// A method for printing out the space. 
+        /// If the game is over (nothing changes anymore) the changed parameter has to be false and a 'game is closed' tag will be printed.
+        /// It also prints out on the console.
+        /// </summary>
+        /// <param name="changed">Decides wheter the endscreen or the next generation will be shown.</param>
+        private void printSpace(Boolean changed)
+        {
+            String output = "";
+            if (changed)
+            {
+                for (int rows = 0; rows < space.GetLength(0); rows++)
+                {
+                    String row = "";
+                    for (int cols = 0; cols < space.GetLength(1); cols++)
+                    {
+                        String token = "";
+                        if (space[rows, cols])
+                        {
+                            //"X"
+                            token = Strings.Default.tokenPositve;
+                        }
+                        else
+                        {
+                            //"O"
+                            token = Strings.Default.tokenNegative;
+                        }
+                        //" | "
+                        row = row + Strings.Default.separator + token;
+                    }
+                    output = output + " \n" + row + Strings.Default.separator;
+                }
+            }
+            else
+            {
+                if (timer != null)
+                {
+                    //stop auto-tick
+                    timer.Stop();
+                }
+                output = "The god was bored. He left.";
+            }
+
+            //write the output
+            displayArea.Text = output;
+            Console.WriteLine(output);
+        }
+
+        /// <summary>
+        /// Updates the alive status of the space and returns whether it changed by updating or not.
+        /// </summary>
+        /// <returns>Returns whether the space changed during the update.</returns>
+        private Boolean updateSpace()
+        {
+            //clone
+            Boolean[,] outputSpace = space.Clone() as Boolean[,];
+
+            //iterate over every cell and decide its fate
+            for (int rows = 0; rows < space.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < space.GetLength(1); cols++)
+                {
+                    //updates the alive status of the specific cell
+                    outputSpace[rows, cols] = playGod(rows, cols);
+                }
+            }
+
+            //a token for deciding whether the space changed
+            Boolean equal =
+                space.Rank == outputSpace.Rank &&
+                Enumerable.Range(0, space.Rank).All(dimension => space.GetLength(dimension) == outputSpace.GetLength(dimension)) &&
+                space.Cast<Boolean>().SequenceEqual(outputSpace.Cast<Boolean>());
+
+            //returns whether the space changed
+            if (equal)
+            {
+                return false;
+            } else
+            {
+                space = outputSpace;
+                return true;
+            }
+            
+        }
+
+        /// <summary>
+        /// Decides the fate of a single cell and returns true for alive or false for dead.
+        /// </summary>
+        /// <param name="row">Defines the row coordinate of the current cell.</param>
+        /// <param name="col">Defines the column coordinate of the current cell.</param>
+        /// <returns>true for alive and false for dead</returns>
+        private Boolean playGod(int row, int col)
+        {
+            int neighboursAlive = 0;
+            int shiftRow = 0;
+            int shiftCol = 0;
+
+            //check in every direction
+
+
+            //--------------------------------------
+            // up, down, left, right
+            //left
+            shiftCol = col - 1;
+            if (shiftCol == -1)
+            {
+                shiftCol = space.GetLength(1)-1;
+            }
+            if (space[row, shiftCol])
+            {
+                neighboursAlive++;
+            }
+            //right
+            shiftCol = col + 1;
+            if (shiftCol == space.GetLength(1))
+            {
+                shiftCol = 0;
+            }
+            if (space[row, shiftCol])
+            {
+                neighboursAlive++;
+            }
+            //up
+            shiftRow = row - 1;
+            if (shiftRow == -1)
+            {
+                shiftRow = space.GetLength(0)-1;
+            }
+            if (space[shiftRow, col])
+            {
+                neighboursAlive++;
+            }
+            //down
+            shiftRow = row + 1;
+            if (shiftRow == space.GetLength(0))
+            {
+                shiftRow = 0;
+            }
+            if (space[shiftRow, col])
+            {
+                neighboursAlive++;
+            }
+
+            //------------------------------------
+            //left up, left down, right up, right down
+            //left up
+            shiftCol = col - 1; //left
+            shiftRow = row - 1; //up
+            if (shiftCol == -1)
+            {
+                shiftCol = space.GetLength(1) - 1;
+            }
+            if (shiftRow == -1)
+            {
+                shiftRow = space.GetLength(0) - 1;
+            }
+            if (space[shiftRow, shiftCol])
+            {
+                neighboursAlive++;
+            }
+
+            //left down
+            shiftCol = col - 1; //left
+            shiftRow = row + 1; //down
+            if (shiftCol == -1)
+            {
+                shiftCol = space.GetLength(1) - 1;
+            }
+            if (shiftRow == space.GetLength(0))
+            {
+                shiftRow = 0;
+            }
+            if (space[shiftRow, shiftCol])
+            {
+                neighboursAlive++;
+            }
+
+            //right up
+            shiftCol = col + 1; //right
+            shiftRow = row - 1; //up
+            if (shiftCol == space.GetLength(1))
+            {
+                shiftCol = 0;
+            }
+            if (shiftRow == -1)
+            {
+                shiftRow = space.GetLength(0) - 1;
+            }
+            if (space[shiftRow, shiftCol])
+            {
+                neighboursAlive++;
+            }
+
+            //right down
+            shiftCol = col + 1; //right
+            shiftRow = row + 1; //down
+            if (shiftCol == space.GetLength(1))
+            {
+                shiftCol = 0;
+            }
+            if (shiftRow == space.GetLength(0))
+            {
+                shiftRow = 0;
+            }
+            if (space[shiftRow, shiftCol])
+            {
+                neighboursAlive++;
+            }
+
+            //check the state and play god
+            if (space[row, col]) //init state "alive"
+            {
+                if (neighboursAlive < 2)
+                {
+                    //kill this cell, too few friends :( ...
+                    return false;
+                }
+                else if (neighboursAlive == 2 || neighboursAlive == 3)
+                {
+                    //stays alive (next line is for readablilty, actually not neccessary)
+                    return true;
+                }
+                else if (neighboursAlive > 3)
+                {
+                    //kill because of overpopulation
+                    return false;
+                } 
+                else
+                {
+                    //don't change a thing
+                    return space[row, col];
+                }
+            }
+            else //init state "dead"
+            {
+                if (neighboursAlive == 3) 
+                {
+                    //resurrect that cell
+                    return true;
+                }
+                else
+                {
+                    //don't change a thing
+                    return space[row, col];
+                }
+            }
+        }
+
+
+        //--------------------------------------------------
+        // Methods for handling the buttons are below.
+        //--------------------------------------------------
+
+
+        /// <summary>
+        /// Handles the event if the "Tick" button was clicked.
+        /// Updates the space array and prints out the solution.
+        /// </summary>
+        /// <param name="sender">Inherited</param>
+        /// <param name="e">Inherited</param>
+        private void BTNTick_Click(object sender, EventArgs e)
+        {
+            Boolean changed = updateSpace();
+            printSpace(changed);
+        }
+
+        /// <summary>
+        /// Handles the event if the "Auto Tick" button was clicked.
+        /// Sets up the timer for ticking every two seconds.
+        /// </summary>
+        /// <param name="sender">Inherited</param>
+        /// <param name="e">Inherited</param>
+        private void BTNAutoTick_Click(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Interval = 2000; // 2 seconds
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        /// <summary>
+        /// Handles the event when a tick is neccessary.
+        /// Performs like the "Tick" button.
+        /// </summary>
+        /// <param name="sender">Inherited</param>
+        /// <param name="e">Inherited</param>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            BTNTick_Click(null, null);
+        }
+
+        /// <summary>
+        /// Handles the event if the "Output" label was clicked.
+        /// Performs like the ´"Tick" button.
+        /// </summary>
+        /// <param name="sender">Inherited</param>
+        /// <param name="e">Inherited</param>
+        private void LabelOutput_Click(object sender, EventArgs e)
+        {
+            BTNTick_Click(null, null);
+        }
+
+        /// <summary>
+        /// Handles the event if the "Reset" button was clicked.
+        /// Resets the space, fills in new random values and prints the new space array.
+        /// </summary>
+        /// <param name="sender">Inherited</param>
+        /// <param name="e">Inherited</param>
+        private void BTNReset_Click(object sender, EventArgs e)
+        {
+            space = new Boolean[space.GetLength(0), space.GetLength(1)];
+            randomizeSpace();
+            printSpace(true);
+        }
+
+        /// <summary>
+        /// Handles the event if the "Exit" button was clicked.
+        /// Exits the program.
+        /// </summary>
+        /// <param name="sender">Inherited</param>
+        /// <param name="e">Inherited</param>
+        private void BTNExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Handles the event if the "OK" button was clicked.
+        /// Updates the size of the space according to the user input.
+        /// Restarts the game.
+        /// </summary>
+        /// <param name="sender">Inherited</param>
+        /// <param name="e">Inherited</param>
+        private void BTNOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //get the control elements
+                Control controlRow = this.Controls["CBRow"];
+                Control controlCol = this.Controls["CBCol"];
+
+                //cast the control elements to ComboBoxes
+                ComboBox cbRow = (ComboBox)controlRow;
+                ComboBox cbCol = (ComboBox)controlCol;
+
+                //update the configuration of the space (rows and columns)
+                GameConfig.Default.rows = Int32.Parse(cbRow.SelectedItem.ToString());
+                GameConfig.Default.cols = Int32.Parse(cbCol.SelectedItem.ToString());
+
+                start();
+            }
+            catch (Exception)
+            {
+                //something went wrong by getting the new size of the space
+                Console.Out.WriteLine("Parsing error by ComboBox!");
+            }
+        }
+    }
+}
